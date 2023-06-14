8d2a716c576194fb830cffe4cd26ac405f9ad00a
sagar rajak <Sagarrajak858@gmail.com>
2023-06-14 11:08:54 +0530

    login page integration

--- src/app/components/input/Input.tsx
+++ src/app/components/input/Input.tsx
@@ -1,53 +1,53 @@
-'use client'
+"use client";
 
 import { FC } from "react";
-import { FieldErrors, FieldValue, FieldValues, UseFormRegister } from "react-hook-form";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 import { BiDollar } from "react-icons/bi";
 
 interface InputProps {
     id: string;
     label: string;
-    register: UseFormRegister<FieldValues>,
-    errors: FieldErrors
+    register: UseFormRegister<FieldValues>;
+    errors: FieldErrors;
     type?: string;
     disabled?: boolean;
     formatPrice?: boolean;
     required?: boolean;
-};
+}
 
 export const Input: FC<InputProps> = ({
     id,
     label,
-    type = 'text',
+    type = "text",
     disabled,
     formatPrice,
     required,
     register,
-    errors
+    errors,
 }) => {
     return (
-        <div className="
+        <div
+            className="
             w-full
             relative
-        ">
-            {
-                formatPrice && (
-                    <BiDollar
-                        size={24}
-                        className="
+        "
+        >
+            {formatPrice && (
+                <BiDollar
+                    size={24}
+                    className="
                           text-neutral-700
                           absolute
                           top-5
                           left-2
                         "
-                    />
-                )
-            }
-            <input 
+                />
+            )}
+            <input
                 id={id}
                 disabled={disabled}
                 required={required}
-                {...register(id, {required})}
+                {...register(id, { required })}
                 placeholder=" "
                 type={type}
                 className={`
@@ -63,9 +63,12 @@ export const Input: FC<InputProps> = ({
                     translate
                     disabled:opacity-70
                     disabled:cursor-not-allowed
-                    ${formatPrice ? 'pl-9' : 'pl-4'}
-                    ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
-                    ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
+                    ${formatPrice ? "pl-9" : "pl-4"}
+                    ${errors[id] ? "border-rose-500" : "border-neutral-300"}
+                    ${errors[id]
+                        ? "focus:border-rose-500"
+                        : "focus:border-black"
+                    }
 
                 `}
             />
@@ -79,16 +82,16 @@ export const Input: FC<InputProps> = ({
                     top-5
                     z-10
                     origin-[0]
-                    ${formatPrice ? 'left-9' : 'left-4'}
+                    ${formatPrice ? "left-9" : "left-4"}
                     peer-placeholder-shown:scale-100 
                     peer-placeholder-shown:translate-y-0 
                     peer-focus:scale-75
                     peer-focus:-translate-y-4
-                    ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
+                    ${errors[id] ? "text-rose-500" : "text-zinc-400"}
                 `}
             >
                 {label}
             </label>
         </div>
     );
-}
+};
