bad68395ad7a49e355aedad36923b5dfbf3054ff
sagar <sagarrajak858@gmail.com>
2023-06-18 01:58:49 +0530

    completed auth work

--- pages/api/auth/[...nextAuth].ts
+++ pages/api/auth/[...nextAuth].ts
@@ -1,8 +1,63 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers"
-import prisma from "@/app/"
+import NextAuth, { Awaitable, RequestInternal, User, AuthOptions } from "next-auth";
+import Providers from "next-auth/providers";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/app/libs/prismadb";
+import bcrypt from "bcrypt";
 
-export default NextAuth({
-  adapter: PrismaAdapter(prisma)
-});
+export const authOption: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "credentails",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      authorize: async (
+        credentials: Record<"email" | "password", string> | undefined,
+        req: Pick<RequestInternal, "query" | "headers" | "body" | "method">
+      ) => {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("Invalid credentails!");
+        }
+        const user = await prisma?.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user.hashedPassword) {
+          throw new Error("Invalid credentails!");
+        }
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentails!");
+        }
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOption);
